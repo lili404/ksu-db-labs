610bbb3692ca67e1c347a4bafb6eb6b49c43cef6
lili404 <140329847+lili404@users.noreply.github.com>
2024-05-26 19:29:58 +0300

    Update zooDB_project.sql

--- zooDB_project.sql
+++ zooDB_project.sql
@@ -1,59 +1,82 @@
-CREATE TABLE medical_record (
+CREATE TABLE health_report (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  animal_id INT NOT NULL,
+  animal_id INT NOT NULL UNIQUE,
   veterinarian_id INT NOT NULL,
-  date DATE NOT NULL,
+  completion_date DATE NOT NULL,
   diagnosis TEXT NOT NULL,
   treatment TEXT NULL,
-  FOREIGN KEY (animalID) REFERENCES animal(id),
-  FOREIGN KEY (veterinarianID) REFERENCES veterinarian(id)
+  FOREIGN KEY (animal_id) REFERENCES animal(id),
+  FOREIGN KEY (veterinarian_id) REFERENCES veterinarian(id)
+);
+
+CREATE TABLE specie (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  type VARCHAR(64) UNIQUE NOT NULL
+);
+
+CREATE TABLE breed (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  type VARCHAR(64) UNIQUE NOT NULL
 );
 
 CREATE TABLE animal (
   id INT PRIMARY KEY,
-  name VARCHAR(128) NOT NULL,
+  specie INT NOT NULL,
+  breed INT NOT NULL,
   birth_date DATE DEFAULT NULL,
-  gender VARCHAR(16) DEFAULT NULL,
+  gender VARCHAR(1) DEFAULT NULL,
   weight DECIMAL(5,2) DEFAULT NULL,
-  health_status INT NOT NULL,
-  FOREIGN KEY (health_status) REFERENCES medical_record(animal_id)
+  health_status INT NOT NULL UNIQUE,
+  FOREIGN KEY (specie) REFERENCES specie(id),
+  FOREIGN KEY (breed) REFERENCES breed(id),
+  FOREIGN KEY (health_status) REFERENCES health_report(animal_id)
 );
 
 CREATE TABLE keeper (
-  id INT PRIMARY KEY,
+  id INT PRIMARY KEY AUTO_INCREMENT,
   full_name VARCHAR(128) NOT NULL,
+  gender VARCHAR(1) NOT NULL,
   shift VARCHAR(5) DEFAULT NULL
+);
+
+CREATE TABLE food_storage (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  type VARCHAR(32) NOT NULL UNIQUE,
+  available DECIMAL(5,2) DEFAULT NULL,
+  units VARCHAR(8) NOT NULL
 );
 
 CREATE TABLE feeding_schedule (
   id INT PRIMARY KEY,
-  animal_id INT NOT NULL,
+  animal_id INT NOT NULL UNIQUE,
   date DATE NOT NULL,
   time TIME NOT NULL,
-  food_type VARCHAR(128) NOT NULL,
+  food_id INT NOT NULL,
   quantity DECIMAL(5,2) NOT NULL,
-  keeper_id  INT NOT NULL,
+  keeper_id INT NOT NULL,
   FOREIGN KEY (animal_id) REFERENCES animal(id),
+  FOREIGN KEY (food_id) REFERENCES food_storage(id),
   FOREIGN KEY (keeper_id) REFERENCES keeper(id)
 );
 
-CREATE TABLE visitors (
+CREATE TABLE visitor (
   id INT PRIMARY KEY AUTO_INCREMENT,
-  full_name VARCHAR(128) NOT NULL,
+  full_name VARCHAR(128) NOT NULL UNIQUE,
+  gender VARCHAR(1) NOT NULL,
   visit_count INT NOT NULL DEFAULT 0
 );
 
-CREATE TABLE tickets (
+CREATE TABLE ticket (
   id INT PRIMARY KEY AUTO_INCREMENT,
   visitor_id INT NOT NULL,
-  buy_date DATE NOT NULL,
-  visit_date DATE NOT NULL,
-  visit_time TIME NOT NULL,
+  buy_date DATETIME NOT NULL,
+  visit_date DATETIME NOT NULL,
   used BOOLEAN NOT NULL DEFAULT FALSE,
   FOREIGN KEY (visitor_id) REFERENCES visitors(id)
 );
 
-
-
-
- 
+CREATE TABLE med_storage (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  type VARCHAR(32) NOT NULL UNIQUE,
+  available DECIMAL(5,2) DEFAULT NULL
+);
