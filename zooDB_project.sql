148c7ed2c418127e85fa3a6c8e222cb6dc714aad
lili404 <140329847+lili404@users.noreply.github.com>
2024-05-27 21:59:55 +0300

    Update zooDB_project.sql

--- zooDB_project.sql
+++ zooDB_project.sql
@@ -178,17 +178,17 @@ VALUES ('Doxycycline', 10.00),
 -- Insert health care info per animal
 INSERT INTO health_care (animal_id, date, diagnosis, medicine_id, quantity, staff_id) VALUES
   -- Lion
-  ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Lion')), CURDATE(), 'Routine checkup', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Jane Smith')),
+  ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Lion')), CURDATE(), 'Healthy', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Jane Smith')),
   -- Tiger
   ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Tiger')), CURDATE(), 'Vaccination', (SELECT id FROM medicine WHERE type = 'Doxycycline'), 1.00, (SELECT id FROM staff WHERE full_name = 'Michael Johnson')),
   -- Gorilla
   ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Gorilla')), CURDATE(), 'Injury treatment', (SELECT id FROM medicine WHERE type = 'Metronidazole'), 0.50, (SELECT id FROM staff WHERE full_name = 'Jane Smith')),
   -- Chimpanzee
-  ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Chimpanzee')), CURDATE(), 'Dental examination', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Michael Johnson')),
+  ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Chimpanzee')), CURDATE(), 'Healthy', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Michael Johnson')),
   -- Orangutan
   ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Orangutan')), CURDATE(), 'Behavioral consultation', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Jane Smith')),
   -- Bear
-  ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Bear')), CURDATE(), 'Weight check', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Jane Smith')),
+  ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Bear')), CURDATE(), 'Healthy', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Jane Smith')),
   -- Giraffe
   ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Giraffe')), CURDATE(), 'Hoof trimming', (SELECT id FROM medicine WHERE type = 'Carprofen'), 2.00, (SELECT id FROM staff WHERE full_name = 'Jane Smith')),
   -- Lemur
@@ -196,7 +196,7 @@ INSERT INTO health_care (animal_id, date, diagnosis, medicine_id, quantity, staf
   -- Zebra
   ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Zebra')), CURDATE(), 'Wound dressing', (SELECT id FROM medicine WHERE type = 'Enrofloxacin'), 1.00, (SELECT id FROM staff WHERE full_name = 'Jane Smith')),
   -- Parrot
-  ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Parrot')), CURDATE(), 'Feather examination', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Michael Johnson'));
+  ((SELECT id FROM animal WHERE specie = (SELECT id FROM specie WHERE type = 'Parrot')), CURDATE(), 'Healthy', NULL, NULL, (SELECT id FROM staff WHERE full_name = 'Michael Johnson'));
 
 -- Insert food types and available quantity
 INSERT INTO food (type, available, units) VALUES
@@ -252,4 +252,211 @@ INSERT INTO ticket (visitor_id, buy_date, visit_date) VALUES
   ((SELECT id FROM visitor WHERE full_name = 'Chris Wilson'), '2024-06-07 13:54:43', '2024-06-08 08:00:00'),
   ((SELECT id FROM visitor WHERE full_name = 'Jessica Garcia'), '2024-06-08 12:39:21', '2024-06-09 08:00:00');
 
+-- QUERIES
+-- 1. List all animal species
+SELECT * FROM specie ORDER BY id;
+-- 1.1. Find total number of animal species
+SELECT COUNT(*) AS totalSpecieCount FROM specie;
 
+-- 2. List all animal breeds
+SELECT * FROM breed ORDER BY id;
+-- 2.1. Find total number of animal breeds
+SELECT COUNT(*) AS totalBreedCount FROM breed ;
+
+-- 3. List all animals
+SELECT * FROM animal ORDER BY id;
+-- 3.1. Find total number of animals
+SELECT COUNT(*) AS totalAnimalCount FROM animal;
+
+-- 4. List all food types
+SELECT * FROM food ORDER BY id;
+-- 4.1. Find total number of food types
+SELECT COUNT(*) AS totalFoodTypeCount FROM food;
+
+-- 5. List feeding schedules
+SELECT * FROM feeding_schedule ORDER BY id;
+-- 5.1. Find total number of feeding schedules
+SELECT COUNT(*) AS totalFeedingScheduleCount FROM feeding_schedule;
+
+-- 6. List all medicine types
+SELECT * FROM medicine ORDER BY id;
+-- 6.1. Find total number of medicine types
+SELECT COUNT(*) AS totalMedicineTypeCount FROM medicine;
+
+-- 7. List health care reports
+SELECT * FROM health_care ORDER BY id;
+-- 7.1. Find total number of health care reports
+SELECT COUNT(*) AS totalHealthCareReportCount FROM health_care;
+
+-- 8. List all staff members
+SELECT * FROM staff ORDER BY id;
+-- 8.1. Find total number of staff members
+SELECT COUNT(*) AS totalStaffMemberCount FROM staff;
+
+-- 9. List all job positions
+SELECT * FROM job_position ORDER BY id;
+-- 9.1. Find total number of job positions
+SELECT COUNT(*) AS totalJobPositionCount FROM job_position;
+
+-- 10. List all salaries
+SELECT * FROM salary ORDER BY id;
+-- 10.1. Find total number of possible salaries(at this zoo)
+SELECT COUNT(*) AS totalSalaryTypeCount FROM salary;
+
+-- 11. List all visitors
+SELECT * FROM visitor ORDER BY id;
+-- 11.1. Find total number of visitors
+SELECT COUNT(*) AS totalVisitorCount FROM visitor;
+
+-- 12. List all bought tickets
+SELECT * FROM ticket ORDER BY id;
+-- 12.1. Find total number of bought tickets
+SELECT COUNT(*) AS totalSoldTicketCount FROM ticket;
+
+-- 13. List all animals that weight >= 200 kg
+SELECT * FROM animal
+WHERE weight >= 200;
+
+-- 14. List all animals whose age > 20 years
+SELECT * FROM animal
+WHERE DATEDIFF(CURDATE(), birth_date) / 365.25 >= 20;
+
+-- 15. List all animals that are male
+SELECT * FROM animal
+WHERE gender = 'M';
+-- 15.1. List all animals that are female
+SELECT * FROM animal
+WHERE gender = 'F';
+
+-- 16. List all animals that are healthy
+SELECT
+  a.id,
+  s.type
+  AS specie,
+  b.type
+  AS breed,
+  a.birth_date,
+  a.gender
+  FROM animal a
+JOIN specie s ON a.specie = s.id
+JOIN breed b ON a.breed = b.id
+JOIN health_care hc ON a.id = hc.animal_id
+WHERE hc.diagnosis = 'Healthy';
+
+-- 16.1 List all animals that are not healthy
+SELECT
+  a.id,
+  s.type
+  AS specie,
+  b.type
+  AS breed,
+  a.birth_date,
+  a.gender
+  FROM animal a
+JOIN specie s ON a.specie = s.id
+JOIN breed b ON a.breed = b.id
+JOIN health_care hc ON a.id = hc.animal_id
+WHERE hc.diagnosis != 'Healthy';
+
+-- 17. List a health care report about gorilla
+SELECT 
+    a.id AS animal_id, 
+    s.type AS specie, 
+    b.type AS breed, 
+    a.birth_date, 
+    a.gender, 
+    a.weight, 
+    hc.date AS health_care_date, 
+    hc.diagnosis, 
+    m.type AS medicine, 
+    hc.quantity AS medicine_quantity, 
+    st.full_name AS veterinarian
+FROM 
+    animal a
+JOIN specie s ON a.specie = s.id
+JOIN breed b ON a.breed = b.id
+JOIN health_care hc ON a.id = hc.animal_id
+JOIN medicine m ON hc.medicine_id = m.id
+JOIN staff st ON hc.staff_id = st.id
+WHERE s.type = 'Gorilla';
+
+-- 18. Find the type and quantity of food that tiger eats per day
+SELECT 
+	s.type AS specie_type,
+    f.type AS food_type, 
+    fs.quantity AS food_quantity
+FROM 
+    animal a
+JOIN specie s ON a.specie = s.id
+JOIN feeding_schedule fs ON a.id = fs.animal_id
+JOIN food f ON fs.food_id = f.id
+WHERE s.type = 'Tiger';
+
+-- 19. List staff whose age is >= 24
+SELECT * FROM staff
+WHERE age >= 24;
+
+-- 20. List all zoo managers
+SELECT * FROM staff
+WHERE position_id = (SELECT id FROM job_position WHERE position = 'Zoo Manager');
+-- 20.1. List all veterinarians
+SELECT * FROM staff
+WHERE position_id = (SELECT id FROM job_position WHERE position = 'Veterinarian');
+-- 20.2. List all keepers
+SELECT * FROM staff
+WHERE position_id = (SELECT id FROM job_position WHERE position = 'Keeper');
+-- 20.3. List all cashiers
+SELECT * FROM staff
+WHERE position_id = (SELECT id FROM job_position WHERE position = 'Cashier');
+
+-- 21. List staff that works in the day shifts
+SELECT * FROM staff
+WHERE shift = 'Day';
+-- 21.1. List staff that works in the night shifts
+SELECT * FROM staff
+WHERE shift = 'Night';
+
+-- 22. List staff whose salary > 20000
+SELECT 
+  s.*,
+  sal.salary
+FROM staff s
+JOIN job_position jp ON s.position_id = jp.id
+JOIN salary sal ON jp.salary = sal.id
+WHERE sal.salary >= 20000;
+-- 22.1. List staff whose salary <= 20000
+SELECT 
+  s.*,
+  sal.salary
+FROM staff s
+JOIN job_position jp ON s.position_id = jp.id
+JOIN salary sal ON jp.salary = sal.id
+WHERE sal.salary <= 20000;
+
+-- 23. Find the average age of staff
+SELECT ROUND(AVG(age)) AS average_age
+FROM staff;
+
+-- 24. Find out which gender visits the zoo the most
+SELECT gender, COUNT(*) AS visit_count
+FROM visitor
+GROUP BY gender
+ORDER BY visit_count DESC
+LIMIT 1;
+
+-- 25. Find the average age of visitor
+SELECT ROUND(AVG(age)) AS average_age
+FROM visitor;
+
+-- 26. Update the ticket 'used' state
+UPDATE ticket
+SET used = TRUE
+WHERE id = 1;
+
+SELECT used FROM ticket WHERE id = 1;
+
+UPDATE ticket
+SET used = FALSE
+WHERE id = 1;
+
+SELECT used FROM ticket WHERE id = 1;
